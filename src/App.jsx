45c32e8862dd32ecabb7270ef7bdd9245515675f
Christian <111076892+i-christian@users.noreply.github.com>
2024-05-25 15:36:09 +0200

    update styles

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ const App = () => {
   const [connected, setConnected] = useState(false);
   const onceRef = useRef(false);
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  const lastMessageRef = useRef(null);
 
   useEffect(() => {
     setMessages([]);
@@ -26,11 +27,11 @@ const App = () => {
     if (onceRef.current) return;
     onceRef.current = true;
 
-    const socket = io("ws://localhost:3000");
+    const socket = io("ws://localhost:8000");
     setSocket(socket);
 
     socket.on("connect", () => {
-      setName(`anon-${socket.id}`);
+      setName(`user-${socket.id}`);
       setConnected(true);
       socket.emit("join", currentRoom);
     });
@@ -38,6 +39,7 @@ const App = () => {
     socket.on("message", (msg) => {
       msg.date = new Date(msg.date);
       setMessages((messages) => [...messages, msg]);
+      scrollToBottom();
     });
 
     socket.on("messages", (msgs) => {
@@ -46,6 +48,7 @@ const App = () => {
         date: new Date(msg.date),
       }));
       setMessages(formattedMessages);
+      scrollToBottom();
     });
   }, []);
 
@@ -53,6 +56,12 @@ const App = () => {
     e.preventDefault();
     socket?.emit("message", { text: input, room: currentRoom });
     setInput("");
+  };
+
+  const scrollToBottom = () => {
+    if (lastMessageRef.current) {
+      lastMessageRef.current.scrollIntoView({ behavior: "smooth" });
+    }
   };
 
   const rooms = ["General", "Programming", "Games", "Sports"];
@@ -67,24 +76,24 @@ const App = () => {
         setSidebarOpen={setSidebarOpen}
       />
       <SidebarDesktop rooms={rooms} currentRoom={currentRoom} setCurrentRoom={setCurrentRoom} />
-      <div className="h-screen p-4 bg-ctp-crust flex flex-col flex-grow justify-end">
-        <div className="bg-ctp-base rounded-t-lg flex-grow">
-          <div className="sticky top-0 z-40 flex items-center gap-x-6 bg-ctp-mantle px-2 sm:px-6 lg:hidden">
-            <button type="button" className="-m-2.5 p-2.5 text-gray-400 lg:hidden" onClick={() => setSidebarOpen(true)}>
+      <section className="h-screen p-4 bg-ctp-crust flex flex-col flex-grow justify-end overflow-hidden">
+        <header className="bg-ctp-base rounded-t-lg sticky top-0 z-40">
+          <div className="hidden lg:flex justify-center items-center gap-x-6 bg-ctp-mantle px-2 sm:px-6">
+            <h1 className="text-2xl text-white font-bold my-4">{currentRoom}</h1>
+          </div>
+          <div className="lg:hidden flex justify-between items-center bg-ctp-mantle px-2 sm:px-6">
+            <button type="button" className="-m-2.5 p-2.5 text-gray-400" onClick={() => setSidebarOpen(true)}>
               <span className="sr-only">Open sidebar</span>
               <Bars3Icon className="h-6 w-6" aria-hidden="true" />
             </button>
-            <div className="flex-1 text-sm font-semibold leading-6 text-white">
-              <h1 className="text-2xl text-white font-bold py-4">{currentRoom}</h1>
-            </div>
+            <h1 className="text-sm font-semibold leading-6 text-white">{currentRoom}</h1>
           </div>
-          <h1 className="hidden lg:block text-2xl text-center text-white font-bold my-4">{currentRoom}</h1>
-          <MessageList messages={messages} />
-        </div>
+        </header>
+        <MessageList messages={messages} currentUser={name} lastMessageRef={lastMessageRef} />
         <ChatInput input={input} setInput={setInput} sendMessage={sendMessage} />
-      </div>
+      </section>
     </main>
   );
-}
+};
 
 export default App;
